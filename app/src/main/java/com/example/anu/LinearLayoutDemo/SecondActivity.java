dafbaa50eb0cbb5959502765f45614cdda63d8eb
anupamDebnath <anupam.amicable@gmail.com>
2016-06-21 12:16:55 +0600

    Linear Layout and Relative Layout test

---
+++ app/src/main/java/com/example/anu/LinearLayoutDemo/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.anu.LinearLayoutDemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Anu on 6/7/2016.
+ */
+public class SecondActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
